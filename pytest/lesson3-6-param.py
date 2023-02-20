c9bc8c8da01bd4850269753f31790be129691347
Olga Yachmeneva <yaolga@MacBook-Air-Olga-2.local>
2023-02-20 11:43:21 +0300

    lesson3-6, global configuration and tests parametrization

---
+++ pytest/lesson3-6-param.py
@@ -0,0 +1,12 @@
+import time
+
+import pytest
+from selenium.webdriver.common.by import By
+
+
+@pytest.mark.parametrize('language', ["ru", "en-gb"])
+def test_guest_should_see_login_link(browser, language):
+    link = f"http://selenium1py.pythonanywhere.com/{language}/"
+    browser.get(link)
+    browser.find_element(By.CSS_SELECTOR, "#login_link")
+    time.sleep(5)
---
+++ pytest/test_conftest.py
@@ -0,0 +1,12 @@
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+def test_guest_should_see_login_link(browser):
+    browser.get(link)
+    browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+def test_guest_should_see_login_link_fail(browser):
+    browser.get(link)
+    browser.find_element(By.CSS_SELECTOR, "#magic_link")
